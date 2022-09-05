cc1915d16b2fcb6babca4733b12cb1fbd7842528
nguyentea <81692943+nguyentea@users.noreply.github.com>
2022-09-05 09:44:54 +0700

    Create ScanQR.dart

---
+++ lib/view/ScanQR.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter/material.dart';
+
+class ScanQRCode extends StatefulWidget {
+  const ScanQRCode({Key? key}) : super(key: key);
+
+  @override
+  _ScanQRCodeState createState() => _ScanQRCodeState();
+}
+
+class _ScanQRCodeState extends State<ScanQRCode> {
+
+  String qrResult = "Scanner Data will appear hear";
+
+  Future<void> scanQR() async {
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      if (!mounted) return;
+      setState(() {
+        this.qrResult = qrCode.toString();
+      });
+    } on PlatformException {
+      qrResult = 'Fail to read QR Code';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('QR Code Scanner'),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30,),
+            Text('$qrResult', style: TextStyle(color: Colors.black),),
+            SizedBox(height: 30,),
+            ElevatedButton(onPressed: scanQR, child: Text('Scan Code')),
+          ],
+        ),
+      ),
+    );
+  }
+}
