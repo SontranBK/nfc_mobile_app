1bb5f8a9a2593bd206880b730986e65d5f87dec9
nguyentea <81692943+nguyentea@users.noreply.github.com>
2022-09-10 12:33:28 +0700

    update code

---
+++ lib/src/web/flutter_qr_stub.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:app/src/types/camera.dart';
+
+Widget createWebQrView(
+    {onPlatformViewCreated, onPermissionSet, CameraFacing? cameraFacing}) =>
+    const SizedBox();
